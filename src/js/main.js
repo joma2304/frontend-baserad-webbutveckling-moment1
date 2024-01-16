c71d110fb131a5efdfcfd6dc25e5991613eb9a87
Johan Magnusson <joma2304@student.miun.se>
2024-01-16 21:43:47 +0100

    Lagt till text + klocka i menyn

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,23 @@ function toggleMenu() {
         navMenuEl.style.display = "none";
     }
 }
+
+//startar funktion vid inladdning av sida
+onload=startTime();
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
